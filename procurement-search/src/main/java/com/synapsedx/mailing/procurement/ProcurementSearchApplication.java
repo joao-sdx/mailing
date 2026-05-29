0bb5ebe8264bc6310b9db411a4b435c216a5d830
joao.violante <joao.violante@synapsedx.com>
2026-05-29 11:05:21 +0100

    feat(procurement-search): scaffold module with pom, Taskfile, and application config

---
+++ procurement-search/src/main/java/com/synapsedx/mailing/procurement/ProcurementSearchApplication.java
@@ -0,0 +1,17 @@
+package com.synapsedx.mailing.procurement;
+
+import com.synapsedx.mailing.procurement.config.BoampProperties;
+import com.synapsedx.mailing.procurement.config.ProcurementProperties;
+import com.synapsedx.mailing.procurement.config.TedProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({ProcurementProperties.class, TedProperties.class, BoampProperties.class})
+public class ProcurementSearchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProcurementSearchApplication.class, args);
+  }
+}
